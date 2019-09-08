553bed9e5665dd41966e47ed07c31fdf28032efe
Shola Ayeni <ayenisholah@yahoo.com>
2019-09-08 18:03:21 +0100

    install bootstrap dependency

---
+++ fundamentals/src/app/event-app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'event-app',
+  template: `
+    <h2>Hello World</h2>
+    <img src="assets/images/basic-shield.png" />
+  `
+})
+export class EventsAppComponent {
+  title = 'fundamentals';
+}
