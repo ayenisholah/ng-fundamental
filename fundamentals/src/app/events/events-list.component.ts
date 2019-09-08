5badad1dece72185ae9ebad5eaa59b5f55c161a4
Shola Ayeni <ayenisholah@yahoo.com>
2019-09-08 19:47:04 +0100

    created events list component

---
+++ fundamentals/src/app/events/events-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-events-list',
+  templateUrl: './events-list.component.html',
+  styleUrls: ['./events-list.component.css']
+})
+export class EventsListComponent {
+  event = {
+    id: 1,
+    name: 'Angular Connect',
+    time: '10:00 am',
+    date: '9/26/2036',
+    price: 599.99,
+    imageUrl: '/assets/images/angularconnect-shield.png',
+    location: {
+      address: '1057 DT',
+      city: 'London',
+      country: 'England'
+    }
+  };
+}
