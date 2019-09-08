1e0dd6127244d2fd0ba4b20b58cc98e607744e9e
Shola Ayeni <ayenisholah@yahoo.com>
2019-09-08 19:47:37 +0100

    WIP

--- fundamentals/src/app/event-app.component.ts
+++ fundamentals/src/app/event-app.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   template: `
     <h2>Hello World</h2>
     <img src="assets/images/basic-shield.png" />
+    <app-events-list></app-events-list>
   `
 })
 export class EventsAppComponent {
