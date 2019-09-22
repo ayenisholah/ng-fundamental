17a39b6c1dae5a7a1aaf7da4e61a79a6530a355d
Shola Ayeni <ayenisholah@yahoo.com>
2019-09-22 21:32:31 +0100

    styling active links

--- fundamentals/src/app/nav/nav.component.ts
+++ fundamentals/src/app/nav/nav.component.ts
@@ -4,16 +4,12 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styles: [`
-    .nav.navbar-nav {font-size: 15px}
-    #searchForm {margin-right: 100px}
-    @media (max-width: 1200px) {searchForm {display: none}}
+    .nav.navbar-nav { font-size: 15px }
+    #searchForm { margin-right: 100px }
+    @media (max-width: 1200px) { searchForm { display: none } }
+    li > a.active { color: #F97924 }
   `]
 })
-export class NavComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class NavComponent {
 
 }
