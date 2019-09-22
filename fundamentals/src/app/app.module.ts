88560e0e30a63742bf0f78ad2987434cc8f28f94
Shola Ayeni <ayenisholah@yahoo.com>
2019-09-22 22:26:51 +0100

    lazy loading feature modules

--- fundamentals/src/app/app.module.ts
+++ fundamentals/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
-import { HttpClientModule } from '@angular/common/http';
 
 import { EventsAppComponent } from './event-app.component';
 import { EventsListComponent } from './events/event-list/events-list.component';
@@ -29,7 +28,6 @@ import { EventListResolver } from './events/event-list-resolver.service';
     Error404Component
   ],
   imports: [
-    HttpClientModule,
     BrowserModule,
     CommonModule,
     BrowserAnimationsModule,
